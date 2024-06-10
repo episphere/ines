e26420bc87e02a09292a8f111191a2371112f226
Jonas Almeida <jalmeida@mathbiol.org>
2024-06-10 16:38:06 -0400

    run clock

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log(` index.js loaded\n ${Date()}`)
 
+// running the clock
+setInterval(function(){
+    helloWorld.innerHTML=`at <span style="font-size:x-large;color:darkgreen;background-color:yellow">${new Date().toLocaleTimeString()}</span>`
+},1000)
