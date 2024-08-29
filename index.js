6a04720b314a58eb4b0c84aec1f0b665d25e796e
InesNDuarte <inesnduarte4@gmail.com>
2024-08-29 19:34:06 +0100

    Update index.js
    print lala

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ console.log(` index.js loaded\n ${Date()}`)
 
 // running the clock
 setInterval(function(){
-    helloWorld.innerHTML=`at <span style="font-size:x-large;color:darkgreen;background-color:yellow">${new Date().toLocaleTimeString()}</span>`
+    helloWorld.innerHTML=`at <span style="font-size:x-large;color:darkgreen;background-color:darkgreen">${new Date().toLocaleTimeString()}</span>`
 },1000)
+
+System.out.print('lala')
