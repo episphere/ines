b319695e1fa98dab274609529d2622ebbe22592a
Jonas Almeida <jalmeida@mathbiol.org>
2024-09-05 10:58:16 -0400

    soma

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-console.log(` index.js loaded\n ${Date()}`)
-
+console.log(` index.js loaded\n ${Date()}`);
+cleanVal=function(v){
+    if(!v){
+        v=0
+    }else{
+        v=parseFloat(v)
+    }
+    return v
+}
 // running the clock
-setInterval(function(){
+setTimeout(function(){
     helloWorld.innerHTML=`at <span style="font-size:x-large;color:darkgreen;background-color:yellow">${new Date().toLocaleTimeString()}</span>`
+    console.log('val1:',val1)
+    val1.onkeyup=val2.onkeyup=function(ev){
+        soma.innerHTML=cleanVal(val1.value)+cleanVal(val2.value)
+        console.log('ev1:',ev)
+    }
 },1000)
-let xx=document.getElementById('xx')
-xx.onkeyup=function(ev){console.log(x.value)}
+//let x=document.getElementById('xx');
+// document.getElementById('val1').onkeyup=function(ev){console.log(ev)}
